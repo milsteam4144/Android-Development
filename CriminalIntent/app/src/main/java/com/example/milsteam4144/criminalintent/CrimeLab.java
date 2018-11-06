30133ba97b27d91be41eef88cef6f1c9366c936a
Mallory Milstead <milsteam4144@student.faytechcc.edu>
2018-11-06 14:28:26 -0500

    Added CrimeLab class to hold list of crime objects
    Singleton class that holds the list

---
+++ CriminalIntent/app/src/main/java/com/example/milsteam4144/criminalintent/CrimeLab.java
@@ -0,0 +1,43 @@
+package com.example.milsteam4144.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+//This is a singleton class--only one instance of the class is created & it is global
+public class CrimeLab {
+    private static CrimeLab sCrimeLab; // Declare the field for the singleton object-"s" means static
+    private List<Crime> mCrimes; //Create an empty list to hold the Crime objects
+
+    //Private Constructor
+    private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+    }
+
+    //Getter for the list mCrimes, returns the entire list
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    //Returns the specific crime if the given id matches that of a crime in the list
+    public Crime getCrimeByID(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+
+    //Method to return the singleton object (sCrimeLab) that holds the list mCrimes
+    public static CrimeLab get(Context context) //This will return the (1) CrimeLab object
+    {
+        if (sCrimeLab == null) //If the object doesn't exist, create it
+        {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+}
