0b14a9265ee7540e7cb256f94d8889ff9c22d6e1
Mallory Milstead <milsteam4144@student.faytechcc.edu>
2018-12-04 14:31:54 -0500

    Connect to Flickr API and create JSON objects

---
+++ PhotoGallery/app/src/main/java/com/example/milsteam4144/photogallery/GalleryItem.java
@@ -0,0 +1,12 @@
+package com.example.milsteam4144.photogallery;
+
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    @Override
+    public String toString(){
+        return mCaption;
+    }
+}
--- PhotoGallery/app/src/main/java/com/example/milsteam4144/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/example/milsteam4144/photogallery/PhotoGalleryFragment.java
@@ -1,14 +1,20 @@
 package com.example.milsteam4144.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+
 public class PhotoGalleryFragment extends Fragment {
+
+    private static final String TAG = "PhotoGalleryFragment";
 
     private RecyclerView mPhotoRecyclerView;
 
@@ -20,6 +26,7 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Override
@@ -30,6 +37,14 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
         return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void>{
+        @Override
+        protected Void doInBackground(Void...params){
+            new FlickrFetchr().fetchItems();
+            return null;
+        }
     }
 
 }
